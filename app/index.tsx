5399c222cccf83715f6db2129f928a048e9a8379
markm <markmutisya72@gmail.com>
2025-07-09 07:04:57 -0700

    Indexing Changes

--- app/index.tsx
+++ app/index.tsx
@@ -2,7 +2,10 @@ import { ImageBackground, StyleSheet, Text, TouchableOpacity, View } from "react
 import React from "react";
 import { Link, Stack } from "expo-router";
 import { LinearGradient } from "expo-linear-gradient";
-
+import { Colors } from "@/constants/Colors";
+import { Ionicons } from "@expo/vector-icons";
+import Google from '@/assets/images/google-logo.svg'
+import Animated, { FadeInDown, FadeInRight } from "react-native-reanimated";
 type Props = {};
 
 const WelcomeScreen = (props: Props) => {
@@ -19,17 +22,56 @@ const WelcomeScreen = (props: Props) => {
       <LinearGradient 
       colors={["transparent", 'rgba(255, 255, 255, 0.9)','rgba(255, 255, 255, 1)']} 
       style={styles.background}>
-      <Text>Welcome Screen</Text>
+      <View style={styles.wrapper}>
+      <Animated.Text style={styles.title}
+      entering={FadeInRight.delay(300).duration(300)}>
+        ShopAZ</Animated.Text>
+      
+      <Animated.Text style={styles.description}
+      entering={FadeInRight.delay(500).duration(300)}>
+        The One Stop Access To All Your Commodites, 
+        Your Item is Just A Click Away..✈️
+      </Animated.Text>
+
+      <View style={styles.socialLoginWrapper}>
+      <Animated.View entering={FadeInDown.delay(300).duration(500)}>
+      <Link href={"/signup"} asChild>
+        <TouchableOpacity style={styles.button}>
+          <Ionicons 
+          name="mail-outline"
+          size={20}
+          color={Colors.black}/>
+          <Text style={styles.btnTxt}>Continue With Email</Text>
+        </TouchableOpacity>
+      </Link>
+      </Animated.View>
+      
+      <Animated.View entering={FadeInDown.delay(700).duration(500)}>
+        <TouchableOpacity style={styles.button}>
+          <Google width={20} height={20}/>
+          <Text style={styles.btnTxt}>Continue With Google</Text>
+        </TouchableOpacity>    
+      </Animated.View>
+      
+      <Animated.View entering={FadeInDown.delay(1100).duration(500)}>
+        <TouchableOpacity style={styles.button}>
+          <Ionicons 
+          name="logo-apple"
+          size={20}
+          color={Colors.black}/>
+          <Text style={styles.btnTxt}>Continue With Apple</Text>
+        </TouchableOpacity> 
+      </Animated.View>     
+      </View>
+
+      <Text style={styles.loginTxt}> Already Have an Account? {" "} 
       <Link href={"/signin"} asChild>
         <TouchableOpacity>
-          <Text>Go to SignIn Screen</Text>
+          <Text style={styles.loginTxtSpan}>SignIn</Text>
         </TouchableOpacity>
       </Link>
-      <Link href={"/signup"} asChild>
-        <TouchableOpacity>
-          <Text>Go to SignUp Screen</Text>
-        </TouchableOpacity>
-      </Link>
+      </Text>
+      </View>
       </LinearGradient>
 
     </View>
@@ -46,7 +88,6 @@ const styles = StyleSheet.create({
     justifyContent: "center",
     alignItems: "center",
   },
-
   background: {
     flex: 1,
     position: "absolute",
@@ -55,6 +96,60 @@ const styles = StyleSheet.create({
     right: 0,
     bottom: 0,
     justifyContent: "flex-end",
+  },
+  wrapper: {
+    paddingBottom: 50,
+    paddingHorizontal: 20,
+    alignItems: "center",
+  },
+  title: {
+    fontSize: 25,
+    color: Colors.primary,
+    fontWeight: "800",
+    letterSpacing: 2.4,
+    marginBottom: 10
+  },
+  description: {
+    alignItems: "center",
+    fontSize:14,
+    color: Colors.gray,
+    letterSpacing: 1.2,
+    lineHeight: 30,
+    marginBottom: 20,
+    textAlign: "center"
+  },
+  socialLoginWrapper: {
+    alignSelf: "stretch",
+  },
+  button: {
+    flexDirection: "row",
+    padding: 10,
+    borderColor: Colors.gray,
+    borderWidth: StyleSheet.hairlineWidth,
+    borderRadius: 20,
+    marginBottom: 10,//25
+    marginTop: 1,
+    alignItems: "center",
+    justifyContent: "center",
+    gap: 10
+  },
+  btnTxt: {
+    fontSize: 14,
+    fontWeight: "600",
+    color: Colors.black,
 
-  }
+  },
+  loginTxt: {
+    marginTop: 30,
+    fontSize: 14,
+    fontWeight: "600",
+    color: Colors.black,
+    lineHeight: 24,
+  },
+  loginTxtSpan: {
+    marginTop: 30,
+    fontWeight: "600",
+    color: Colors.primary,
+  },
+
 });
