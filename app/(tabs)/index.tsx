e87d247bb73e2236ea5d9c7144c9089bd13ab244
markm <markmutisya72@gmail.com>
2025-07-12 15:24:59 -0700

    Continuation

--- app/(tabs)/index.tsx
+++ app/(tabs)/index.tsx
@@ -1,12 +1,33 @@
-import { StyleSheet, Text, View } from 'react-native'
-import React from 'react'
+import { FlatList, StyleSheet, Text, View } from 'react-native'
+import React, { useEffect, useState } from 'react'
+import axios from 'axios';
+import { ProductType } from '@/types/type';
 
 type Props = {}
 
 const HomeScreen = (props: Props) => {
+  const [products, setProducts] = useState<ProductType[]>([]);
+  const [isLoading, setIsLoading] = useState<boolean>(true);
+
+  useEffect(() => {
+    getProducts();
+  }, []);
+
+  const getProducts = async () => {
+    const URL = 'http://localhost:8000/products';
+    const response = await axios.get(URL);
+
+    console.log(response.data);
+    setProducts(response.data);
+    setIsLoading(false);
+  }
   return (
     <View style={styles.container}>
       <Text>Home Screen</Text>
+      <FlatList 
+      data={products} 
+      keyExtractor={(item) => item.id.toString()} 
+      renderItem={({index, item}) => <Text>{item.title}</Text>}/>
     </View>
   )
 }
@@ -16,7 +37,5 @@ export default HomeScreen
 const styles = StyleSheet.create({
   container: {
     flex: 1,
-    justifyContent: 'center',
-    alignItems: 'center'
   }
 })
