6d72c1f3c2693759d5c010a6534a7611105ef94e
markm <markmutisya72@gmail.com>
2025-07-14 05:32:08 -0700

    Flash Sales Updates

--- app/(tabs)/index.tsx
+++ app/(tabs)/index.tsx
@@ -1,20 +1,24 @@
 import { FlatList, Image, StyleSheet, Text, Touchable, TouchableOpacity, View } from 'react-native'
 import React, { useEffect, useState } from 'react'
 import axios from 'axios';
-import { ProductType } from '@/types/type';
+import { CategoryType, ProductType } from '@/types/type';
 import { Stack } from 'expo-router';
 import Header from '@/components/Header';
 import ProductItem from '@/components/ProductItem';
 import { Colors } from '@/constants/Colors';
 import ProductList from '@/components/ProductList';
+import Categories from '@/components/Categories';
+import FlashSale from '@/components/FlashSale';
 
 type Props = {}
 
 const HomeScreen = (props: Props) => {
   const [products, setProducts] = useState<ProductType[]>([]);
+  const [categories, setCategories] = useState<CategoryType[]>([]);
   const [isLoading, setIsLoading] = useState<boolean>(true);
 
   useEffect(() => {
+    getCategories();
     getProducts();
   }, []);
 
@@ -22,8 +26,17 @@ const HomeScreen = (props: Props) => {
     const URL = 'http://localhost:8000/products';
     const response = await axios.get(URL);
 
-    console.log(response.data);
+    //console.log(response.data);
     setProducts(response.data);
+    setIsLoading(false);
+  }
+
+  const getCategories = async () => {
+    const URL = 'http://localhost:8000/categories';
+    const response = await axios.get(URL);
+
+    //console.log(response.data);
+    setCategories(response.data);
     setIsLoading(false);
   }
   return (
@@ -32,9 +45,10 @@ const HomeScreen = (props: Props) => {
       headerShown: true,
       header: () => (
         <Header/>
-      ),
-         
+      ),         
       }}/>
+      <Categories categories={categories}/>
+      <FlashSale />
       <ProductList products={products}/>
     </>
   )
---
+++ components/FlashSale.tsx
@@ -0,0 +1,124 @@
+import { View, Text, Image, FlatList, TouchableOpacity, StyleSheet } from 'react-native'
+import React, { useEffect, useState } from 'react'
+import { Colors } from '@/constants/Colors'
+import { Ionicons } from '@expo/vector-icons'
+
+type Props = {}
+
+const FlashSale = (props: Props) => {
+    const saleEndDate = new Date();
+    saleEndDate.setDate(saleEndDate.getDate() + 2);
+    saleEndDate.setHours(23, 59, 59)
+
+    const [timeUnits, setTimeUnits] = useState({
+        days: 0,
+        hours: 0,
+        minutes: 0,
+        seconds: 0
+    });
+
+    useEffect(() => {
+        const calculateTimeUnits = (timeDifference: number) => {
+            const seconds = Math.floor(timeDifference / 1000);
+            setTimeUnits({
+                days: Math.floor((seconds % (365 * 24 * 60 * 60)) / (24 * 60 * 60)),
+                hours: Math.floor((seconds % (24 * 60 * 60)) / (60 * 60)),
+                minutes: Math.floor((seconds % (60 * 60)) / 60),
+                seconds: seconds % 60,
+            });
+        };
+
+        const updateCountdown = () => {
+            const currentDate = new Date().getTime();
+            const expiryTime = saleEndDate.getTime();
+            const timeDifference = expiryTime - currentDate;
+
+            if (timeDifference <= 0) {
+                // Countdown Finished
+                calculateTimeUnits(0);
+            } else {
+                calculateTimeUnits (timeDifference);
+            }
+        }
+
+        updateCountdown();
+        const interval = setInterval(updateCountdown, 1000);
+
+        return () => clearInterval(interval);
+      }, []);
+
+      const formatTime = (time: number) => {
+          return time.toString().padStart(2, '0');
+      }
+  return (
+    <View style={styles.container}>
+      <View style={styles.titleWrapper}>
+            <View style={styles.timerWrapper}>
+                <Text style={styles.title}>Flash Sale</Text>
+            <View style={styles.timer}>
+                <Ionicons name="time-outline" size={16} color={Colors.black}/>
+                <Text style={styles.timerText}>{`${formatTime(timeUnits.days)}:${formatTime(timeUnits.hours)}:${formatTime(timeUnits.minutes)}:${formatTime(timeUnits.seconds)}`}</Text>
+            </View>
+            </View>
+            <TouchableOpacity>
+                <Text style={styles.titleBtn}>See All</Text>
+            </TouchableOpacity>
+        </View>
+    </View>
+  )
+}
+
+export default FlashSale;
+
+const styles = StyleSheet.create({
+  container: {
+      marginBottom: 20
+  },
+  titleWrapper: {
+    flexDirection: 'row',
+    justifyContent: 'space-between',
+    marginHorizontal: 20
+  },
+  title: {
+    fontSize: 10,
+    fontWeight: '600',
+    letterSpacing: 0.6,
+    color: Colors.black
+  },
+  titleBtn: {
+    fontSize: 10,
+    fontWeight: '500',
+    letterSpacing: 0.6,
+    color: Colors.black
+  },
+  item: {
+    marginVertical: 10,
+    gap: 5,
+    alignItems: 'center',
+    marginLeft: 20
+  },
+  itemImg: {
+    width: 50,
+    height: 50,
+    borderRadius: 30,
+    backgroundColor: Colors.cynan
+  },
+  timerWrapper: {
+    flexDirection: 'row',
+    alignItems: 'center',
+    gap: 10
+  },
+  timer: {
+    flexDirection: 'row',
+    gap: 5,
+    backgroundColor: Colors.highlight,
+    paddingHorizontal: 8,
+    paddingVertical: 5,
+    borderRadius: 12
+  },
+  timerText: {
+    color: Colors.black,
+    fontWeight: '500'
+  }
+  
+})
